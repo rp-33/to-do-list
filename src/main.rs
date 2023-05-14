d6cf996b71053ffa45ea942c07fa41ffa2c7ecf3
primero <primero@News-iMac.local>
2023-05-14 04:02:48 -0400

    show all task

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ struct Task{
 
 impl Task{
     fn create(task: String,done: bool)->Task{
-        Task { task, done }
+        return Task { task, done };
+    }
+
+    fn show_all(tasks: &mut Vec<Task>){
+        for item in tasks.iter() { 
+            println!("tarea=> {}",item.task)
+        }
     }
 
     fn find(&self){
@@ -16,12 +22,16 @@ impl Task{
 }
 
 
-fn create_task(task: String,done: bool){
-    let todo_instance = Task::create(task,done);
-    println!("la tarea es => {}",todo_instance.task)
+fn create_task(tasks: &mut Vec<Task>,task: String,done: bool){
+    let new_task = Task::create(task,done);
+    tasks.push(new_task);
+    println!("la tarea creada es=>{}",tasks[tasks.len() - 1].task);
 }
 
 fn main() {
+    //creamos un nuevo vector mutable de tipo Task
+    let mut tasks: Vec<Task> = Vec::new(); 
+    //iteramos
     loop {
         let mut action = String::new();
         let mut task = String::new();
@@ -30,18 +40,23 @@ fn main() {
         println!("Que accion deseas realizar?");
         io::stdin().read_line(&mut action);
       
-        
-        if action.trim().to_owned() == "break" {
+        action = action.trim().to_owned();
+
+        if action == "break" {
             break;
         }
 
-        println!("Ingrese el nombre de la tarea");
-        io::stdin().read_line(&mut task);
+        if action == "create" {
+            println!("Ingrese el nombre de la tarea");
+            io::stdin().read_line(&mut task);
+            task = task.trim().to_owned();
+        }
         
         println!("action {}",action);
 
         match action.trim().to_owned().as_ref() {
-            "create" => create_task(task.to_string(),false),
+            "create" => create_task(&mut tasks,task.to_string(),false),
+            "show all" => Task::show_all(&mut tasks),
             _ => println!("la opcion es invalida"),
         }
         
